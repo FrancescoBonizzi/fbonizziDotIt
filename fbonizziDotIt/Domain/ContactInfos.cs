d01b9171dc53475cb82e8336a48b07a2d52ccf94
Francesco Bonizzi <fbonizzi@hotmail.it>
2018-06-29 13:52:21 +0200

    Social media

--- fbonizziDotIt/Domain/ContactInfos.cs
+++ fbonizziDotIt/Domain/ContactInfos.cs
@@ -6,7 +6,8 @@
         public ContactInfo Github { get; set; }
         public ContactInfo Facebook { get; set; }
         public ContactInfo Twitter { get; set; }
-        public ContactInfo Patreon { get; set; }
+        public ContactInfo Instangram { get; set; }
         public ContactInfo YouTube { get; set; }
+        public ContactInfo Android { get; set; }
     }
 }
